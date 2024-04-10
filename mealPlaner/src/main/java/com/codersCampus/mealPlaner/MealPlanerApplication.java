751ebbabeba7869742be61c2901bc9e95cffd653
K4ss0 <mikalandrs@gmail.com>
2024-04-09 21:38:19 -0700

    First instance

---
+++ mealPlaner/src/main/java/com/codersCampus/mealPlaner/MealPlanerApplication.java
@@ -0,0 +1,13 @@
+package com.codersCampus.mealPlaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealPlanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealPlanerApplication.class, args);
+	}
+
+}
---
+++ mealPlaner/src/test/java/com/codersCampus/mealPlaner/MealPlanerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codersCampus.mealPlaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealPlanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
